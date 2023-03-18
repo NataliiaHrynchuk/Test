f127bcd5a4e3f3c11aaedff01579e589350eedba
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2023-03-18 23:11:41 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,20 +3,20 @@ import { useState, useEffect } from "react";
 
 export const Button = () => {
     const [isFollowing, setIsFollowing] = useState(() => JSON.parse(window.localStorage.getItem('isFollowing')) ?? false);
-    const [text, setText] = useState(() => JSON.parse(window.localStorage.getItem('text')) ?? "follow",);
-    
+        
     const handleClick = () => {
         setIsFollowing(!isFollowing);
-        isFollowing ? setText("following") : setText("follow");
     };
 
     useEffect(() => {
-        window.localStorage.setItem('text', JSON.stringify(text));
         window.localStorage.setItem('isFollowing', JSON.stringify(isFollowing));
-    }, [text, isFollowing]);
+    }, [ isFollowing]);
 
     
     return <SC.FollowBtn
         onClick={handleClick}
-        type="button">{text}</SC.FollowBtn>
+        type="button"
+    >
+        {isFollowing ? 'following' : 'follow'}
+    </SC.FollowBtn>
 }
